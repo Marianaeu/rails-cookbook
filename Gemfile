b53f6e9fccf346a18dddacc6b9faa650f5d754c0
Mariana Pacuraru <pacuraru.mariana@yahoo.com>
2025-05-12 17:18:56 +0100

    created the categories

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem "simple_form"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
@@ -59,4 +60,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "cleaning DB..."
+Recipe.destroy_all
+puts "creating new recipes..."
+Recipe.create(title: "Spaghetti Carbonara", description: "Classic Italian pasta with eggs, cheese, pancetta, and pepper.", image_url: "https://images.unsplash.com/photo-1579631542720-3a87824fff86?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", rating: 4.7),
+Recipe.create(title: "Chicken Tikka Masala", description: "Creamy tomato-based Indian curry with grilled chicken.", image_url: "https://images.unsplash.com/photo-1603894584373-5ac82b2ae398?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", rating: 4.8),
+Recipe.create(title: "Avocado Toast", description: "Sourdough toast topped with avocado, chili flakes, and a drizzle of olive oil.", image_url: "https://images.unsplash.com/photo-1632217471220-a661da2ae319?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", rating: 4.5),
+Recipe.create(title: "Chocolate Lava Cake", description: "Decadent dessert with a warm, gooey chocolate center.", image_url: "https://images.unsplash.com/photo-1511911063855-2bf39afa5b2e?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", rating: 4.9)
+puts "#{Recipe.count} recipes created!"
